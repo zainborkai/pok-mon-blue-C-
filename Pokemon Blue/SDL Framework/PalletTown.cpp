acdbe0502fe2392175f079863511804f1737bdac
98345 <zacharycarreiro@gmail.com>
2018-09-21 17:05:21 -0400

    House Maps Added
    Make Viridian Forest Building
    
    Make Collision
    
    Add Actors

--- Pokemon Blue/SDL Framework/PalletTown.cpp
+++ Pokemon Blue/SDL Framework/PalletTown.cpp
@@ -1,34 +1,31 @@
-#include "PalletTown.h"
-
-PalletTown::PalletTown() :
-	AnimatedTexture("CroppedMap.png", 800, 576, 1600, 1000, 1, 1, VERTICAL) {
-	Pos(Vector2(0, 0));
-	WrapMode(LOOP);
-}
-
-PalletTown::~PalletTown() {}
 
 
+int PlayerHouseFloorOne[8][8] = { {0,0,0,0,0,0,0,0},
+								  {0,0,0,0,0,0,0,0},
+								  {0,0,0,0,0,0,0,0},
+								  {0,0,0,0,0,0,0,0},
+								  {0,0,0,0,0,0,0,0},
+								  {0,0,0,0,0,0,0,0},
+								  {0,0,0,0,0,0,0,0},
+								  {0,0,0,0,0,0,0,0}, };
 
-void PalletTown::Update() {
-	//Translate(Vector2(-.1f, -.1f));
-}
+int PlayerHouseFloorTwo[8][8] = { { 0,0,0,0,0,0,0,0 },
+								  { 0,0,0,0,0,0,0,0 },
+								  { 0,0,0,0,0,0,0,0 },
+								  { 0,0,0,0,0,0,0,0 },
+							  	  { 0,0,0,0,0,0,0,0 },
+								  { 0,0,0,0,0,0,0,0 },
+								  { 0,0,0,0,0,0,0,0 },
+								  { 0,0,0,0,0,0,0,0 }, };
 
-void PalletTown::Render() {
-	Vector2 pos = Pos(WORLD);
-	Vector2 scale = Scale(WORLD);
-
-	this->mRenderRect.x = pos.x;
-	this->mRenderRect.y = pos.y;
-
-	//mRenderRect.x = (int)(pos.x - mWidth * scale.x * 0.5f);
-	//mRenderRect.y = (int)(pos.y - mWidth * scale.y * 0.5f);
-
-	// Scales the width and height according to the scale of the GameEntity
-	//mRenderRect.w - (int)(mWidth * scale.x);
-	//mRenderRect.h - (int)(mHeight * scale.y);
-
-	mGraphics->DrawTexture(this->mTex, (mClipped) ? &this->mClipRect : NULL, &this->mRenderRect, Rotation(WORLD));
-}
+int PalletHouse[8][8] = { { 0,0,0,0,0,0,0,0 },
+						  { 0,0,0,0,0,0,0,0 },
+						  { 0,0,0,0,0,0,0,0 },
+						  { 0,0,0,0,0,0,0,0 },
+						  { 0,0,0,0,0,0,0,0 },
+						  { 0,0,0,0,0,0,0,0 },
+						  { 0,0,0,0,0,0,0,0 },
+						  { 0,0,0,0,0,0,0,0 }, };
 
 
+//--------------------------------------------------//
