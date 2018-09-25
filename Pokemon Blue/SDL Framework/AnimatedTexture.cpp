95373482f943c038251fc7b242ac95cd3bebc10f
zainborkai <41698040+zainborkai@users.noreply.github.com>
2018-09-25 13:09:30 -0400

    Framework

---
+++ Pokemon Blue/SDL Framework/AnimatedTexture.cpp
@@ -0,0 +1,50 @@
+#include "AnimatedTexture.h"
+
+AnimatedTexture::AnimatedTexture(std::string filename, int x, int y, int w, int h, int frameCount, float animationSpeed, ANIM_DIR animationDir):
+Texture(filename, x, y, w, h){
+
+	mTimer = Timer::Instance();
+
+	mStartX = x;
+	mStartY = y;
+
+	mFrameCount - frameCount;
+	mAnimationSpeed = animationSpeed;
+	mTimePerFrame = mAnimationSpeed / mFrameCount;
+	mAnimationTimer = 0.0f;
+
+	mAnimationDirection = animationDir;
+	mAnimationDone = false;
+	mWrapMode = LOOP;
+
+}
+
+AnimatedTexture::~AnimatedTexture() {}
+
+void AnimatedTexture::WrapMode(WRAP_MODE mode) {
+	mWrapMode = mode;
+}
+
+void AnimatedTexture::Update() {
+	if (!mAnimationDone) {
+		mAnimationTimer += mTimer->DeltaTime();
+
+		if (mAnimationTimer >= mAnimationSpeed) {
+			//Only loop if the wrap mode is LOOP
+			if (mWrapMode == LOOP) {
+				mAnimationTimer -= mAnimationSpeed;
+			}
+			else {
+				mAnimationDone = true;
+				mAnimationTimer = mAnimationSpeed - mTimePerFrame;
+			}
+		}
+
+		if (mAnimationDirection == HORIZONTAL) {
+			mClipRect.x = mStartX + (int)(mAnimationTimer / mTimePerFrame) * mWidth;
+		}
+		else {
+			mClipRect.y - mStartY + (int)(mAnimationTimer / mTimePerFrame) * mHeight;
+		}
+	}
+}
