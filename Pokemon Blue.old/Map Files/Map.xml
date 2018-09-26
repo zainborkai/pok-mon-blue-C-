397b8658199d4992e48eb5e22ae33fb4ebd3e125
98345 <zacharycarreiro@gmail.com>
2018-09-26 16:49:58 -0400

    Deleted .old

--- Pokemon Blue.old/Map Files/Map.xml
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="CroppedMap" tilewidth="16" tileheight="16" tilecount="17424" columns="88">
- <image source="../Debug/Assets/CroppedMap.png" width="1408" height="3168"/>
-</tileset>
--- Pokemon Blue.old/Map Files/Pallet Town.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Pallet Town" tilewidth="16" tileheight="16" tilecount="360" columns="20">
- <image source="../../../Users/Zachary.Carreiro/Desktop/Pokemon Blue Maps/PalletTown_Map.png" width="320" height="288"/>
-</tileset>
