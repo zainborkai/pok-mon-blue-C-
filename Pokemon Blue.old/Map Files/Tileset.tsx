b2233d46ad6d0385ced99f6b925cc09d2cacb6a8
98345 <zacharycarreiro@gmail.com>
2018-09-26 16:18:27 -0400

    Removed .old

--- Pokemon Blue.old/Map Files/Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Tileset" tilewidth="16" tileheight="16" tilecount="1558" columns="38">
- <image source="../Debug/Assets/Tileset.png" width="608" height="656"/>
-</tileset>
